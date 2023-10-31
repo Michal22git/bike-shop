8fd0ed7952a075a40d2a341f420474946ac33fd6
Michal22git <michalfiszer15@gmail.com>
2023-10-31 03:28:26 +0100

    Added email order confirmation

--- bike_shop/settings.py
+++ bike_shop/settings.py
@@ -134,3 +134,9 @@ LOGIN_URL = 'users:login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
--- shop/signals.py
+++ shop/signals.py
@@ -1,9 +1,12 @@
 import os
+
+from django.core.mail import send_mail
 from django.db.models import Sum
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.template.loader import render_to_string
 
-from .models import Product, CartedItem
+from .models import Product, CartedItem, Order
 
 
 @receiver(post_delete, sender=Product)
@@ -22,3 +25,27 @@ def update_cart_totals(sender, instance, **kwargs):
     cart.total_items = total_items or 0
     cart.total_price = total_price or 0
     cart.save()
+
+
+@receiver(post_save, sender=Order)
+def send_order_confirmation_email(sender, instance, created, **kwargs):
+    if created:
+        order = instance
+        subject = 'Order Confirmation'
+        template_name = 'email/order_confirmation.html'
+
+
+        context = {
+            'order': order,
+        }
+
+        html_content = render_to_string(template_name, context)
+
+        send_mail(
+            subject,
+            '',
+            '',
+            [order.user.email],
+            html_message=html_content,
+            fail_silently=False,
+        )
